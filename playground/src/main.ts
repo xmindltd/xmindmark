e3945bf3205b381d4800a032f5caa346c5075939
jim lyn <parrotdance95@gmail.com>
2022-04-10 01:32:24 +0800

    feat: use monaco-editor in playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,15 +2,16 @@ import { parseXMindMarkToXMindFile } from '../../src'
 import { downloadFile, loadFileAsText } from './loader'
 import { loadExternalScript } from './loader'
 import { renderMapByString } from './map'
+import * as monaco from 'monaco-editor'
 
 let openedFileName: string = ''
+let editor: monaco.editor.IStandaloneCodeEditor
 
 const renderEngineDownloadUrl = 'https://assets.xmind.net/snowbrush/snowbrush-2.47.0.js'
 
-function initScript() {
+function initRenderEngine() {
   const result = document.getElementById('result') as HTMLDivElement
   const convert = document.getElementById('convert') as HTMLButtonElement
-  const input = document.getElementById('input') as HTMLTextAreaElement
 
   result.classList.add('loading')
   convert.classList.add('loading')
@@ -19,34 +20,53 @@ function initScript() {
     result.classList.remove('loading')
     convert.classList.remove('loading')
 
-    if (input.value.length > 0) {
-      renderMapByString(input.value)
+    const existContent = editor.getValue()
+    if (existContent.length > 0) {
+      renderMapByString(existContent)
     }
   })
 
   return
 }
 
+function initEditor() {
+  const input = document.getElementById('input') as HTMLDivElement
+  
+  editor = monaco.editor.create(input, {
+    language: 'markdown',
+    wordWrap: 'on',
+    minimap: { enabled: false },
+    fontSize: 16,
+    // hide gutter aside line number
+    lineNumbers: 'on',
+    lineDecorationsWidth: 6,
+    lineNumbersMinChars: 0,
+    folding: false,
+    // hide vertical line of ruler
+    overviewRulerBorder: false,
+  })
+
+  window.addEventListener('resize', () => {
+    const { width, height } = input.getBoundingClientRect()
+    input.style.setProperty('width', `${width}px`)
+    input.style.setProperty('height', `${height}px`)
+    editor.layout()
+  })
+}
+
 function initView() {
   const fileSelect = document.getElementById('file-select') as HTMLInputElement
-  const input = document.getElementById('input') as HTMLTextAreaElement
+  const input = document.getElementById('input') as HTMLDivElement
   const convert = document.getElementById('convert') as HTMLButtonElement
 
   let timeoutToken: ReturnType<typeof setTimeout>
 
   input.addEventListener('keydown', (e) => {
-    // Allow input tab in textarea
-    if (e.key === 'Tab') {
-      e.preventDefault()
-      const startPos = input.selectionStart
-      input.value = input.value.substring(0, startPos) + '    ' + input.value.substring(input.selectionEnd)
-      input.selectionStart = startPos + 4
-    }
-
     clearTimeout(timeoutToken)
     timeoutToken = setTimeout(() => {
-      if (input.value.length > 0) {
-        renderMapByString(input.value)
+      const content = editor.getValue()
+      if (content.length > 0) {
+        renderMapByString(content)
       }
     }, 500);
   })
@@ -58,7 +78,7 @@ function initView() {
       input.classList.add('loading')
 
       const result = await loadFileAsText(file)
-      input.value = result
+      editor.setValue(result)
       renderMapByString(result)
       input.classList.remove('loading')
       fileSelect.files = null
@@ -66,14 +86,16 @@ function initView() {
   })
 
   convert.addEventListener('click', async () => {
-    const arrayBuffer = await parseXMindMarkToXMindFile(input.value)
-    const downloadFileName = openedFileName.length > 0 ? openedFileName : input.value.split('\n').map(str => str.trim()).filter(str => str.length > 0)[0]
+    const content = editor.getValue()
+    const arrayBuffer = await parseXMindMarkToXMindFile(content)
+    const downloadFileName = openedFileName.length > 0 ? openedFileName : content.split('\n').map(str => str.trim()).filter(str => str.length > 0)[0]
     downloadFile(arrayBuffer, `${downloadFileName}.xmind`)
   })
 }
 
 function main() {
-  initScript()
+  initEditor()
+  initRenderEngine()
   initView()
 }
 
